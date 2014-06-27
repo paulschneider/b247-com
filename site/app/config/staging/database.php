53505d6ef11c00423a96419a0d66c71240332aa4
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-06-27 14:16:30 +0100

    Updated stage DB connection

--- site/app/config/staging/database.php
+++ site/app/config/staging/database.php
@@ -3,7 +3,7 @@
 return [
 	'connections' => [
 		'mysql' => [
-			'database' => 'wfstage_b247_com',
+			'database' => 'wfstage_b247_com_site',
 			'username' => 'root',
 			'password' => 'r]upoyJmL)2t'
 		]

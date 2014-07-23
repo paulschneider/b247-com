3c5e8f46af014b082473a5e79c12272d25993100
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-07-23 12:41:38 +0100

    Test data for default article

--- site/app/views/partials/tplDefaultArticle.blade.php
+++ site/app/views/partials/tplDefaultArticle.blade.php
@@ -22,8 +22,24 @@
 		<p>&nbsp;</p>
 			<p><a href="http://api.wf-staging.co.uk{{ $article['path'] }}" class="btn hide_tablet hide_mobile btn-lg btn-danger">{{ $article['title'] }}</a>
 <a href="b247://article?channel={{ $article['assignment']['subChannel']['id'] }}&amp;category={{ $article['assignment']['category']['id'] }}&amp;article=9999999" class="hide_desktop btn btn-lg btn-danger" rel="nofollow">{{ $article['title'] }}</a>
-	
+
 		<p>&nbsp;</p>
+
+		<p><a href="http://api.wf-staging.co.uk/we-dont-have-an-endpoint-for-the-website-for-listings" class="btn hide_tablet hide_mobile btn-lg btn-danger">Test Channel Listing</a>
+<a href="b247://channel/60/listing?range=day&amp;time=1404172801" class="hide_desktop btn btn-lg btn-danger" rel="nofollow">Test Channel Listing</a>
+
+		<p>&nbsp;</p>
+
+				<p><a href="http://api.wf-staging.co.uk/we-dont-have-an-endpoint-for-the-website-for-listings" class="btn hide_tablet hide_mobile btn-lg btn-danger">Test Category Listing</a>
+<a href="b247://category/19/listing/articles?subChannel=60&amp;range=day&amp;time=1404172801" class="hide_desktop btn btn-lg btn-danger" rel="nofollow">Test Category Listing</a>
+
+		<p>&nbsp;</p>
+
+		<img src="http://www.entrepreneur.com/dbimages/article/h0/how-win-social-media-right-now-silly-cat-pics-dont-cut.jpg" width="305" />
+
+		<p>&nbsp;</p>
+
+
  
 		</table>
  

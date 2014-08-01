46563dd52f449cddd17df3342085ae97aaae6f1a
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-08-01 11:15:59 +0100

    channelType string to lower case

--- site/app/controllers/ArticleController.php
+++ site/app/controllers/ArticleController.php
@@ -16,7 +16,7 @@ Class ArticleController extends BaseController {
 		$data['isMobile'] = false;
 
 		// we need to work out what type of page to display based on the channel type (e.g listing, promotion, directory or article)
-		$data['channelType'] = getChannelType($data['channel']);
+		$data['channelType'] = strtolower(getChannelType($data['channel']));
 
 		// we don't know what type of data we've had returned by the API so just throw it all at the view and let it decide what to use
 		return View::make("articles.template", $data);
